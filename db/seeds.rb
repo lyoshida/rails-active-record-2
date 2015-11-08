689407207a2ccb71c505d06ece4cc2d47185fad8
Luis Yoshida <luis.yoshida@gmail.com>
2015-11-08 16:52:52 -0200

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+fiorina = User.create(username: 'Fiorina', password_digest: '123')
+trump = User.create(username: 'Trump', password_digest: '123')
+carson = User.create(username: 'Carson', password_digest: '123')
+clinton = User.create(username: 'Clinton', password_digest: '123')
+
+fiorina.profile = Profile.create(gender: 'female', first_name: 'Carly', last_name: 'Fiorina', birth_year: 1954)
+trump.profile = Profile.create(gender: 'male', first_name: 'Donald', last_name: 'Trump', birth_year: 1946)
+carson.profile = Profile.create(gender: 'male', first_name: 'Ben', last_name: 'Carson', birth_year: 1951)
+clinton.profile = Profile.create(gender: 'female', first_name: 'Hillary', last_name: 'Clinton', birth_year: 1947)
+
+fiorina_todo = TodoList.create(list_name: 'todo list', list_due_date: Date.today + 1.year, user: fiorina)
+trump_todo = TodoList.create(list_name: 'todo list', list_due_date: Date.today + 1.year, user: trump)
+carson_todo = TodoList.create(list_name: 'todo list', list_due_date: Date.today + 1.year, user: carson)
+clinton_todo = TodoList.create(list_name: 'todo list', list_due_date: Date.today + 1.year, user: clinton)
+
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: fiorina_todo)
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: fiorina_todo)
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: fiorina_todo)
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: fiorina_todo)
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: fiorina_todo)
+
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: trump_todo)
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: trump_todo)
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: trump_todo)
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: trump_todo)
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: trump_todo)
+
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: carson_todo)
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: carson_todo)
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: carson_todo)
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: carson_todo)
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: carson_todo)
+
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: clinton_todo)
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: clinton_todo)
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: clinton_todo)
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: clinton_todo)
+TodoItem.create(due_date: Date.today + 1.year, title: 'item', description: 'description', todo_list: clinton_todo)
